2623dd7631fa1766ec0b900bb8a83ad9b88034a9
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-08 20:30:46 -0500

    Using POST variables

--- sending_email/index.php
+++ sending_email/index.php
@@ -6,30 +6,41 @@
   </head>
   <body>
     <?php
+      // 
+      // $emailTo ="bpeterson1663@gmail.com";
+      // $subject ="First Email Subject";
+      // $body = "Awesome Job!";
+      // $headers="From: bpeterson1663@gmail.com";
+      //
+      // if(mail($emailTo, $subject, $body, $headers)){
+      //     echo "Mail Sent Successfully";
+      // }
+      // else{
+      //   echo "Mail did not send";
+      // }
 
-      $emailTo ="bpeterson1663@gmail.com";
-      $subject ="First Email Subject";
-      $body = "Awesome Job!";
-      $headers="From: bpeterson1663@gmail.com";
+      $names=array("Brady", "Mike", "Nathan");
 
-      if(mail($emailTo, $subject, $body, $headers)){
-          echo "Mail Sent Successfully";
-      }
-      else{
-        echo "Mail did not send";
-      }
 
-      
-      if($_GET["submit"]){
-        if($_GET['name']){//Will return true if name has a value when submited
-          echo "Your name is ".$_GET['name'];
+      if($_POST["submit"]){
+        if($_POST['name']){//Will return true if name has a value when submited
+
+          foreach ($names as $name){
+            if ($_POST["name"]==$name);
+              echo "Your name is in the array as ".$name;
+              $knowYou = 1;
+          }
+        }
+
+        if(!$knowYou){
+          echo "Your name is not in the array. Sorry ".$_POST['name'];
         }
       }else {
         echo "Please enter your name";
       }
 
     ?>
-    <form>
+    <form method="POST">
       <label for="name">Name</label>
       <input name="name" type="text"/>
       <input type="submit" name="submit" value="Submit Your Name"/>
